dfeb98cd6c75c966d2cf8e350ec869d91f733c2c
Matheus Tsa <matheus.tsa@hotmail.com>
2020-06-21 21:57:10 -0300

    Adicionada página Sobre

--- app/src/main/java/com/mtsa/resistcalc/MainActivity.kt
+++ app/src/main/java/com/mtsa/resistcalc/MainActivity.kt
@@ -36,8 +36,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             actMain_btFBK -> startActivity(Intent(this, Entrada::class.java).putExtra("OP", "FBK"))
             actMain_btFPK -> startActivity(Intent(this, Entrada::class.java).putExtra("OP", "FPK"))
             actMain_btADIC -> startActivity(Intent(this, DICEntrada1::class.java))
-//            actMain_btADIC -> startActivity(Intent(this, Testes::class.java))
-            actMain_btSobre -> toast("Em desenvolvimento...")
+            actMain_btSobre -> startActivity(Intent(this, AboutMe::class.java))
         }
     }
 }
